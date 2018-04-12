f887f605a950a9004773cc631038414ac13bfd63
zanekkk <cekarcekar@gmail.com>
2018-04-12 17:42:10 +0200

    first commit

---
+++ app/src/main/java/com/example/zaneta/igandroidapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.zaneta.igandroidapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+ Button UKbutton ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        UKbutton = (Button) findViewById(R.id.UKbutton);
+
+        UKbutton.setOnClickListener((v) ->{
+           openSecondActivity();
+
+        });
+
+    }
+
+    public void openSecondActivity(){
+        Intent intent = new Intent(this,ScrollingActivity.class);
+        startActivity(intent);
+    }
+}
